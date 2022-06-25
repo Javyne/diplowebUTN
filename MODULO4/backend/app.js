2e0fecbc3cebe6113e4f591797526f93e3781c2d
Javier Russo <jrrusso2@gmail.com>
2022-06-25 15:15:31 -0300

    RUTAS_DEL_EJERCICIO

--- MODULO4/backend/app.js
+++ MODULO4/backend/app.js
@@ -6,6 +6,9 @@ var logger = require('morgan');
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
+var contactoRouter = require('./routes/contacto');
+var user_detailRouter = require('./routes/user_detail');
+var testingRouter = require('./routes/testing');
 
 var app = express();
 
@@ -21,6 +24,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
+app.use('/contacto', contactoRouter);
+app.use('/user_detail', user_detailRouter);
+app.use('/testing', testingRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
