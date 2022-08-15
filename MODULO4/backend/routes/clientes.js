ae9358a0d5a149e61365c041309591ac850649c1
Javier Russo <jrrusso2@gmail.com>
2022-08-15 18:37:18 -0300

    correccion middlewares

--- MODULO4/backend/routes/clientes.js
+++ MODULO4/backend/routes/clientes.js
@@ -5,12 +5,6 @@ var router = express.Router();
 //CONTROLADOR
 const { redirectClientes, newClientForm, newClient, editClientForm, clientEdit, clientDelete, findClient } = require('../controllers/clientes');
 
-//MIDDLEWARES
-const { check } = require('express-validator');
-const { fieldsValidate } = require('../middlewares/fieldValidate')
-
-
-
 //* CREATE
 router.get('/new/', newClientForm);
 router.post('/', newClient);
